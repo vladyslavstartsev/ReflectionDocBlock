5422ef5201134d4f28887deaeb97d8348f7b298b
Jaapio <jaap@ijaap.nl>
2020-06-18 23:48:32 +0200

    Fix FQSEN resolving on see,covers,uses
    The See, Covers and Use tags can reference also methods, properties and
    constants. Which means that the FqsenResolver cannot handle those properly.
    This patch fixes that issue.

--- src/DocBlock/Tags/Covers.php
+++ src/DocBlock/Tags/Covers.php
@@ -19,6 +19,8 @@ use phpDocumentor\Reflection\Fqsen;
 use phpDocumentor\Reflection\FqsenResolver;
 use phpDocumentor\Reflection\Types\Context as TypeContext;
 use Webmozart\Assert\Assert;
+use function array_key_exists;
+use function explode;
 use function preg_split;
 
 /**
@@ -55,9 +57,22 @@ final class Covers extends BaseTag implements Factory\StaticMethod
         Assert::isArray($parts);
 
         return new static(
-            $resolver->resolve($parts[0], $context),
+            self::resolveFqsen($parts[0], $resolver, $context),
             $descriptionFactory->create($parts[1] ?? '', $context)
         );
+    }
+
+    private static function resolveFqsen(string $parts, ?FqsenResolver $fqsenResolver, ?TypeContext $context) : Fqsen
+    {
+        Assert::notNull($fqsenResolver);
+        $fqsenParts = explode('::', $parts);
+        $resolved = $fqsenResolver->resolve($fqsenParts[0], $context);
+
+        if (!array_key_exists(1, $fqsenParts)) {
+            return $resolved;
+        }
+
+        return new Fqsen($resolved . '::' . $fqsenParts[1]);
     }
 
     /**
